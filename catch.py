ee8bbe05efd018e4e1966549698840f68bf80e07
song0905d <songwoosoo1@gmail.com>
2025-11-17 03:00:55 +0900

    Change user_id in DELETE query to emoji

--- catch.py
+++ catch.py
@@ -14,7 +14,7 @@ import sqlite3
 
 conn = sqlite3.connect("robot_game_runs.db")
 cur = conn.cursor()
-cur.execute("DELETE FROM runs WHERE user_id = ?", ("01",))
+cur.execute("DELETE FROM runs WHERE user_id = ?", ("😋",))
 conn.commit()
 conn.close()
 
