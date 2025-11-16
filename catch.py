0f4ded31389804dbdfe249b0b8b4961a54b60b07
song0905d <songwoosoo1@gmail.com>
2025-11-17 01:42:19 +0900

    Update catch.py

--- catch.py
+++ catch.py
@@ -14,7 +14,7 @@ import sqlite3
 
 conn = sqlite3.connect("robot_game_runs.db")
 cur = conn.cursor()
-cur.execute("DELETE FROM runs WHERE user_id = ?", ("ㅏ",))
+cur.execute("DELETE FROM runs WHERE user_id = ?", ("헤헤",))
 conn.commit()
 conn.close()
 
